7e90c68a2d1718cfef18a5e721ef26fa2d9a10ca
Jonathan Palalay <jonopalalay@gmail.com>
2022-02-19 14:50:58 -0600

    added tracks to collection and search

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,31 +3,33 @@ console.log('***** Music Collection *****')
 let collection = []
 
 
-function addToCollection(title, artist, yearPublished){
+function addToCollection(title, artist, yearPublished, tracks){
   let album = {
     title: title,
     artist: artist,
-    yearPublished: yearPublished
+    yearPublished: yearPublished,
+    tracks: tracks
   }
   collection.push(album);
   return album;
 }
 
 
-console.log(addToCollection('Bleed American', 'Jimmy eat World', '2001' ));
-console.log(addToCollection('Hybrid Theory', 'Linkin Park', '2000' ));
-console.log(addToCollection('Late Registration', 'Kanye West', '2005' ));
-console.log(addToCollection('Graduation', 'Kanye West', '2007' ));
-console.log(addToCollection('Forest', 'J Cole', '2014' ));
-console.log(addToCollection('1989', 'Taylor Swift', '2014' ));
+console.log(addToCollection('Bleed American', 'Jimmy eat World', '2001', 'The Middle: 2:46' ));
+console.log(addToCollection('Hybrid Theory', 'Linkin Park', '2000', 'In the End: 3:36'));
+console.log(addToCollection('Late Registration', 'Kanye West', '2005', 'Heard Em Say: 3:24' ));
+console.log(addToCollection('Graduation', 'Kanye West', '2007', 'Flashing Lights: 3:58'));
+console.log(addToCollection('Forest', 'J Cole', '2014', 'Wet Dreamz: 3:59' ));
+console.log(addToCollection('1989', 'Taylor Swift', '2014', 'Shake It Off: 3:39' ));
 console.log(collection);
 
 
 function showCollection(array){
     console.log('The length of the array:', array.length);
     for(let i=0;i<array.length; i++){
-      console.log(`${array[i].title} by ${array[i].artist} published in ${array[i].yearPublished}`);
-    }
+      console.log(`${array[i].title} by ${array[i].artist} published in ${array[i].yearPublished}
+1. ${array[i].tracks}`);
+    }//updated tracks to show up in the collection
 }
 showCollection(collection);
 
@@ -52,18 +54,19 @@ console.log(findByArtist('Kanye West'));
 
 
 
-function search(searchArtist, searchYear){
+function search(searchArtist, searchYear, searchTracks){
   let searchArray = [];
   const searchCriteria = {
     artist: searchArtist,
-    year:  searchYear
+    year:  searchYear,
+    tracks: searchTracks
 }
 
-if(searchCriteria.artist === undefined && searchCriteria.year === undefined){
+if(searchCriteria.artist === undefined && searchCriteria.year === undefined && searchArtist === undefined){
   return collection;
 }
   for(let i=0; i<collection.length; i++){
-    if( collection[i].artist === searchArtist && collection[i].yearPublished === searchYear){
+    if( collection[i].artist === searchArtist && collection[i].yearPublished === searchYear && collection[i].tracks === searchTracks){
     searchArray.push(collection[i]);
       }
     }
@@ -71,6 +74,6 @@ if(searchCriteria.artist === undefined && searchCriteria.year === undefined){
   }
 
 
-console.log(search('Ray Charles', '1957'));
-console.log(search());
-console.log(search('Linkin Park','2000'));
+console.log(search('Ray Charles', '1957')); //shows empty array because no results
+console.log(search()); //returns collection because the search was empty
+console.log(search('Linkin Park','2000', 'In the End: 3:36')); //shows if it is in the collection
